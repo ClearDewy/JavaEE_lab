002b231b86c5eaea3d95a315f44a919b9716c5d8
Suriel <MeltryYa@outlook.com>
2023-11-21 18:57:53 +0800

    yjy

--- chat/src/LoginFail.java
+++ chat/src/LoginFail.java
@@ -2,14 +2,17 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "LoginFail", value = "/login-fail")
 public class LoginFail extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("登录失败,请重试。");
     }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
